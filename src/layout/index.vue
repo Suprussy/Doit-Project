55f6484233559adf3654aeb5606e543cf1533e1b
Suprussy <suprussy73@gmail.com>
2024-08-03 19:14:09 +0900

    07-1 기본 레이아웃 만들기

---
+++ src/layout/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <section id="wrap">
+    <h1 class="blind">웹사이트 제목</h1>
+    <Header></Header>
+    <section id="container">
+      <router-view></router-view>
+    </section>
+    <Footer></Footer>
+  </section>
+</template>
+
+<script>
+import Header from "./components/Header.vue";
+import Footer from "./components/Footer.vue";
+export default {
+  components: {
+    Header,
+    Footer
+  }
+}
+</script>
