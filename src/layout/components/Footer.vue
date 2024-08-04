0c43aa9c98493becca1f0761fb1ba37704aa6c98
Suprussy <suprussy73@gmail.com>
2024-08-04 16:16:23 +0900

    07-5 반응형으로 만들기

--- src/layout/components/Footer.vue
+++ src/layout/components/Footer.vue
@@ -1,20 +1,20 @@
 <template>
   <footer class="mt-auto">
-    <div class="botinfo flex w-full h-12 py-2.5 bg-slate-100 border-y shadow-sm">
-      <div class="botbtngroup flex justify-end items-center w-full max-w-screen-xl mx-auto px-5">
-        <button class="btn-text relative pr-2.5 leading-none">찾아오시는길</button>
-        <button class="btn-text relative px-2.5 leading-none"><span class="block absolute left-0 top-0 w-px h-3 bg-slate-300"></span>개인정보취급방침</button>
-        <button class="btn-text relative px-2.5 leading-none"><span class="block absolute left-0 top-0 w-px h-3 bg-slate-300"></span>이용약관</button>
-        <button class="btn-gray h-7 ml-2.5 px-2.5 bg-slate-600 text-white leading-none">저자신청</button>
-        <button class="btn-gray h-7 ml-2.5 px-2.5 bg-slate-600 text-white leading-none">역자신청</button>
-        <button class="btn-gray h-7 ml-2.5 px-2.5 bg-slate-600 text-white leading-none">교재샘플/강의자료</button>
+    <div class="botinfo flex w-full h-auto px-7 bg-slate-100 border-y shadow-sm md:h-12 md:px-0 md:py-2.5">
+      <div class="botbtngroup flex justify-center items-center flex-wrap w-full max-w-screen-xl mx-auto py-2.5 md:justify-end md:px-5 md:py-0">
+        <button class="btn-text relative w-1/3 mb-2.5 pr-2.5 text-xs leading-none md:w-auto md:mb-0 md:text-base">찾아오시는길</button>
+        <button class="btn-text relative w-1/3 mb-2.5 px-2.5 text-xs leading-none md:w-auto md:mb-0 md:text-base"><span class="block absolute left-0 top-0 w-px h-3 bg-slate-300"></span>개인정보취급방침</button>
+        <button class="btn-text relative w-1/3 mb-2.5 px-2.5 text-xs leading-none md:w-auto md:mb-0 md:text-base"><span class="block absolute left-0 top-0 w-px h-3 bg-slate-300"></span>이용약관</button>
+        <button class="btn-gray min-w-20 h-6 ml-0 px-2.5 bg-slate-600 text-xs text-white leading-none md:min-w-min md:h-7 md:ml-2.5 md:text-base">저자신청</button>
+        <button class="btn-gray min-w-20 h-6 ml-2.5 px-2.5 bg-slate-600 text-xs text-white leading-none md:min-w-min md:h-7 md:text-base">역자신청</button>
+        <button class="btn-gray min-w-20 h-6 ml-2.5 px-2.5 bg-slate-600 text-xs text-white leading-none md:min-w-min md:h-7 md:text-base">교재샘플/강의자료</button>
       </div>
     </div>
     <div class="footerinfo flex items-center relative w-full max-w-screen-xl mx-auto px-5 py-4">
-      <div class="footer-logo"><img src="/images/footer-logo.png" alt="이지스퍼블리싱 로고" title="이지스퍼블리싱" class="w-32"></div>
+      <div class="footer-logo self-start pt-1 md:self-auto md:pt-0"><img src="/images/footer-logo.png" alt="이지스퍼블리싱 로고" title="이지스퍼블리싱" class="w-20 md:w-32"></div>
       <div class="footer-sub flex-1 pl-5 text-sm">
         <address>
-          서울시 마포구 잔다리로 109 이지스퍼블리싱 <span>Tel 02-1234-12345</span>
+          서울시 마포구 잔다리로 109 이지스퍼블리싱 <span class="block md:inline-block">Tel 02-1234-12345</span>
         </address>
         <p>Email email@email.com</p>
         <p>통신판매업 신고 : 2020-서울강남-001234</p>
--- src/layout/components/Header.vue
+++ src/layout/components/Header.vue
@@ -8,16 +8,27 @@
           </button>
         </div>
         <div class="system">
-          <button class="login">로그인</button>
-          <button class="login"><span class="text-slate-300">ㆍ</span>회원가입</button>
+          <button class="login w-6 h-6 mr-3.5 lg:w-auto lg:h-auto lg:mr-0">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 lg:hidden">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M18 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0ZM3 19.235v-.11a6.375 6.375 0 0 1 12.75 0v.109A12.318 12.318 0 0 1 9.374 21c-2.331 0-4.512-.645-6.374-1.766Z" />
+            </svg>
+            <span class="sr-only lg:not-sr-only">로그인</span>
+          </button>
+          <button class="login w-6 h-6 lg:w-auto lg:h-auto">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 lg:hidden">
+              <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+            </svg>
+            <span class="hidden text-slate-300 lg:inline-block">ㆍ</span>
+            <span class="sr-only lg:not-sr-only">회원가입</span>
+          </button>
         </div>
       </div>
     </div>
     <nav class="relative py-2 bg-red-600 z-30">
       <div class="contentbox w-full max-w-screen-xl mx-auto px-5">
         <ul class="flex">
-          <li v-for="(item, index) in menulists" :key="index" v-on:click="goToPage(item.link)" class="flex-1 text-center">
-            <button v-html="item.menutext" class="text-lg font-semibold text-white"></button>
+          <li v-for="(item, index) in menulists" :key="index" v-on:click="goToPage(item.link)" class="flex-auto pr-2.5 text-center md:flex-1 md:pr-0 last:pr-0">
+            <button v-html="item.menutext" class="text-sm font-semibold text-white md:text-lg"></button>
           </li>
         </ul>
       </div>
