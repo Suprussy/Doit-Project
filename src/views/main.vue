259ff6f2de27d3b382defc8ebc67f69bd063e313
Suprussy <suprussy73@gmail.com>
2024-08-05 11:29:12 +0900

    09-1 메인 콘텐츠 레이아웃 만들기

--- src/views/main.vue
+++ src/views/main.vue
@@ -1,3 +1,20 @@
 <template>
-  <section class="maincontents">메인 영역</section>
+  <section class="maincontents">
+    <section class="mainbanner h-[350px] overflow-hidden text-center shadow-sm"><img src="/images/mainimg.png" alt=""></section>
+    <section class="newbook py-10">
+      <h1 class="maintit mb-5 text-center text-4xl font-bold text-slate-700">
+        새로 나온 도서<span class="block mt-2.5 text-lg text-slate-300">분야별로 새로 나온 도서를 만나보세요</span>
+      </h1>
+    </section>
+    <section class="searchbook">
+      <h1 class="maintit mb-5 text-center text-4xl font-bold text-slate-700">
+        도서 검색<span class="block mt-2.5 text-lg text-slate-300">찾고자 하는 도서명을 검색해 주세요</span>
+      </h1>
+    </section>
+    <section class="bannermenu">
+    </section>
+    <section class="notice">
+      <h1 class="maintit mb-5 text-center text-4xl font-bold text-slate-700">공지사항</h1>
+    </section>
+  </section>
 </template>
